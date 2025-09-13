f6856f21f9900fc7935d1539f425e0b7d9092549
Reg Nott <reg.nott@outlook.com>
2025-09-13 17:08:04 +0100

    AppBar text colour changed

--- lib/main.dart
+++ lib/main.dart
@@ -83,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.primary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title,style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.onPrimary),),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
